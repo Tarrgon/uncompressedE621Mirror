9bfec86a870dccf5f523458c7bd63647dc4df5a4
DontTalkToMeThx <extras@is-just-a.dev>
2024-03-29 12:10:22 -0400

    Bluesky support and fix artstation

--- scripts/observers.js
+++ scripts/observers.js
@@ -1,3 +1,38 @@
+function waitForElm(selector, tester) {
+  return new Promise((resolve) => {
+    if (tester) {
+      for (let ele of document.querySelectorAll(selector)) {
+        if (tester(ele)) return resolve(ele)
+      }
+    } else {
+      let ele = document.querySelector(selector)
+      if (ele) return resolve(ele)
+    }
+
+    const observer = new MutationObserver(() => {
+      if (tester) {
+        for (let ele of document.querySelectorAll(selector)) {
+          if (tester(ele)) {
+            observer.disconnect()
+            return resolve(ele)
+          }
+        }
+      } else {
+        let ele = document.querySelector(selector)
+        if (ele) {
+          observer.disconnect()
+          return resolve(ele)
+        }
+      }
+    })
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true
+    })
+  })
+}
+
 async function sendBlobs(blobs, source) {
   if (blobs.length == 0) return
 
@@ -8,7 +43,7 @@ async function sendBlobs(blobs, source) {
 
   for (let blob of blobs) {
     if (blob.size >= 100 * 1024 * 1024) continue
-    binaryBlobs.push({binary: [...new Uint8Array(await blob.arrayBuffer())], type: blob.type})
+    binaryBlobs.push({ binary: [...new Uint8Array(await blob.arrayBuffer())], type: blob.type })
   }
 
   chrome.runtime.sendMessage({ binaryBlobs, source })
@@ -68,6 +103,104 @@ async function twitterObserver() {
   })
 }
 
+async function blueskyObserver() {
+  let enabled = (await chrome.storage.sync.get("enabled"))?.enabled ?? true
+
+  let currentBlobs = new Map()
+
+  const observer = new MutationObserver((mutationList) => {
+    for (let mutation of mutationList) {
+      let button
+      if ((button = mutation.target.querySelector("[aria-label='Publish post']")) != null && !capturedButtons.includes(button)) {
+        capturedButtons.push(button)
+
+        let customButton = button.cloneNode(true)
+        customButton.firstElementChild.firstElementChild.innerText = `Mirror (${enabled ? "ON" : "OFF"})`
+        button.before(customButton)
+        capturedButtons.push(customButton)
+
+        customButton.addEventListener("click", async () => {
+          enabled = !enabled
+          customButton.firstElementChild.firstElementChild.innerText = `Mirror (${enabled ? "ON" : "OFF"})`
+
+          await chrome.storage.sync.set({
+            enabled: enabled
+          })
+        })
+
+        button.addEventListener("click", async (e) => {
+          if (!enabled) return
+
+          let data = await chrome.storage.sync.get("key")
+          if (!data.key || currentBlobs.size == 0) return
+
+          sendBlobs(currentBlobs.values().toArray(), "bluesky")
+        })
+
+        let capturedInputs = []
+
+        let nextFiles = []
+
+        const fileInputObserver = new MutationObserver((list) => {
+          for (let mutation of list) {
+            if (mutation.addedNodes.length > 0 && !capturedInputs.includes(mutation.addedNodes[0]) &&
+              mutation.addedNodes[0].nodeName == "INPUT" && mutation.addedNodes[0].type == "file" && mutation.addedNodes[0].style.display == "none") {
+              mutation.addedNodes[0].addEventListener("change", () => {
+                nextFiles = Array.from(mutation.addedNodes[0].files)
+              })
+            }
+          }
+        })  
+
+        fileInputObserver.observe(document.body, {
+          childList: true,
+          subtree: false
+        })
+
+        let topParent = null
+
+        const newFileObserver = new MutationObserver((list) => {
+          for (let mutation of list) {
+            if (mutation.addedNodes.length > 0) {
+              for (let node of mutation.addedNodes) {
+                if (node.nodeName == "DIV" && node.parentElement.getAttribute("data-expoimage") == "true") {
+                  if (currentBlobs.has(node.parentNode.parentNode)) continue
+                  if (topParent == null) topParent = node.parentNode.parentNode.parentNode.parentNode
+                  currentBlobs.set(node.parentNode.parentNode, nextFiles.shift())
+                }
+              }
+            } else if (mutation.removedNodes.length > 0) {
+              if (mutation.target == topParent && topParent.childNodes.length == 0) {
+                currentBlobs.clear()
+                continue
+              }
+              let blobNodes = currentBlobs.keys().toArray()
+              for (let node of mutation.removedNodes) {
+                for (let blobNode of blobNodes) {
+                  if (blobNode == node || node.contains(blobNode)) {
+                    currentBlobs.delete(node)
+                    break
+                  }
+                }
+              }
+            }
+          }
+        })
+
+        newFileObserver.observe(document.querySelector("[data-testid='composePostView']"), {
+          childList: true,
+          subtree: true
+        })
+      }
+    }
+  })
+
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true
+  })
+}
+
 async function furaffinityObserver() {
   let enabled = (await chrome.storage.sync.get("enabled"))?.enabled ?? true
 
@@ -102,24 +235,67 @@ async function furaffinityObserver() {
         })
       })
 
-      button.addEventListener("click", async (e) => {
-        if (!enabled) return
+      button.addEventListener("click",)
+    }
+  })
 
-        let data = await chrome.storage.sync.get("key")
-        if (!data.key) return
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true
+  })
+}
 
-        let allImages = Array.from(document.getElementById("submissionFileDragDropArea").querySelectorAll("[src^='blob:']")).map(e => e.src)
-        let allBlobs = []
+async function artstationObserver() {
+  let enabled = (await chrome.storage.sync.get("enabled"))?.enabled ?? true
 
-        for (let image of allImages) {
-          const blob = await (await fetch(image)).blob()
-          if (blob.size >= 100 * 1024 * 1024) continue
-          allBlobs.push(blob)
-        }
+  let currentBlobs = new Map()
 
-        if (allBlobs.length == 0) return
+  let handler = async (e) => {
+    if (!enabled) return
 
-        sendBlobs(allBlobs, "furaffinity")
+    let data = await chrome.storage.sync.get("key")
+    if (!data.key) return
+
+    if (currentBlobs.size == 0) return
+
+    sendBlobs(currentBlobs.values().toArray(), "artstation")
+  }
+
+  let save = await waitForElm(".btn", (e) => e.innerText == "Save")
+  let publish = await waitForElm(".btn", (e) => e.innerText == "Publish")
+
+  let customButton = save.cloneNode(true)
+  customButton.innerHTML = ""
+  customButton.innerText = `Mirror (${enabled ? "ON" : "OFF"})`
+  customButton.type = null
+  save.before(customButton)
+
+  customButton.addEventListener("click", async (e) => {
+    e.preventDefault()
+    e.stopImmediatePropagation()
+    enabled = !enabled
+    customButton.innerText = `Mirror (${enabled ? "ON" : "OFF"})`
+
+    await chrome.storage.sync.set({
+      enabled: enabled
+    })
+  })
+
+  save.addEventListener("click", handler)
+  publish.addEventListener("click", handler)
+
+  const observer = new MutationObserver(async () => {
+    let assets = document.querySelectorAll("project-asset")
+    for (let asset of assets) {
+      if (currentBlobs.has(asset)) continue
+      let url = asset.querySelector(".asset-download > a").href
+      if (url.startsWith("unsafe")) continue
+      currentBlobs.set(asset, await (await fetch(url)).blob())
+
+      let removeButton = asset.querySelector(".btn-blank.remove")
+      removeButton.addEventListener("click", () => {
+        if (asset.parentElement != null) return
+        currentBlobs.delete(asset)
       })
     }
   })
@@ -131,6 +307,9 @@ async function furaffinityObserver() {
 }
 
 (() => {
+  console.log("EXTENSION LOADED!")
   if (window.location.href.includes("x.com") || window.location.href.includes("twitter.com")) twitterObserver()
+  else if (window.location.href.includes("bsky.app")) blueskyObserver()
   else if (window.location.href.includes("furaffinity.net")) furaffinityObserver()
+  else if (window.location.href.includes("artstation.com")) artstationObserver()
 })();
